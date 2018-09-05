bb0d55d79a7e882d8ced66a3cb48ccb0c5960695
Sidrit Trandafili <forge405@gmail.com>
2018-09-05 15:21:15 +0200

    moved composer status to a different repo

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -8,6 +8,11 @@ Nova.booting((Vue, router) => {
             name: 'nova-installed-packages',
             path: '/nova-installed-packages',
             component: require('./components/Index'),
+        },
+        {
+            name: 'nova-installed-packages-detail',
+            path: '/nova-installed-packages/:packageName',
+            component: require('./components/Show'),
         }
     ])
 })
