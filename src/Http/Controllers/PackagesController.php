7ca2716d84b4883e51c08fdd9ce85b65aa71c752
Sidrit Trandafili <forge405@gmail.com>
2018-09-05 15:17:14 +0200

    moved composer status to a different repo

--- src/Http/Controllers/PackagesController.php
+++ src/Http/Controllers/PackagesController.php
@@ -2,9 +2,7 @@
 
 namespace Strandafili\NovaInstalledPackages\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-use Strandafili\NovaInstalledPackages\Jobs\InstallPackage;
 use Strandafili\NovaInstalledPackages\Utils\NovaPackagesFinder;
 
 class PackagesController extends Controller
@@ -12,14 +10,5 @@ class PackagesController extends Controller
     public function index(NovaPackagesFinder $novaPackagesFinder)
     {
         return $novaPackagesFinder->all();
-    }
-
-    public function store(Request $request)
-    {
-        dispatch(
-            new InstallPackage(request('package'), request('packageKey'))
-        );
-
-        return response(null, 204);
     }
 }
